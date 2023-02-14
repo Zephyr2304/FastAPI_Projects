ec1641409fa3b45f6c447a291f4c298dd76eb04a
Zephyr2304 <aayushsingh225.as@gmail.com>
2023-02-15 00:24:44 +0530

    deleted master branch

--- Project_3/todo_app.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from fastapi import FastAPI
-from enum import Enum
-from pydantic import BaseModel
-from uuid import UUID
-
-
-app = FastAPI(title="Project_3",description="This is the todo application with various features of fastapi",docs_url="/docs",redoc_url="/redoc")
