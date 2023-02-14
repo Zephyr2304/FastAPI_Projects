46598a692149323f113c934f4c6c25f5b92de4a7
Zephyr2304 <aayushsingh225.as@gmail.com>
2023-02-14 23:57:45 +0530

    started project_2 and updated some filenames

---
+++ Project_4/main.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from fastapi import FastAPI
+from enum import Enum
+from pydantic import BaseModel
+from uuid import UUID
+
+
+app = FastAPI(title="Project_4",description="This is the Full Stack todo application with authorization,frontend,backend and database",docs_url="/docs",redoc_url="/redoc")
