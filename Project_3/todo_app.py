959ebf24634c0d975a493b304b7aa038cff5bcbb
Zephyr2304 <aayushsingh225.as@gmail.com>
2023-03-04 17:44:19 +0530

    started project_3

--- Project_3/todo_app.py
+++ Project_3/todo_app.py
@@ -3,10 +3,14 @@ from fastapi import FastAPI
 from enum import Enum
 from pydantic import BaseModel
 from uuid import UUID
-
+from database import engine
+import models
 
 app = FastAPI(
     title="Project_3",
     description="This is the todo application with various features of fastapi",
     docs_url="/docs",
     redoc_url="/redoc")
+
+
+models.Base.metadata.create_all(bind=engine)
